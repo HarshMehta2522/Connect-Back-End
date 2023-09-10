dd7ae32c9eeb50b35c690ea9f2aab47c4472731a
HarshMehta2522 <maheta.hrs2522@gmail.com>
2023-09-10 22:47:04 +0530

    update fix

--- index.js
+++ index.js
@@ -8,16 +8,42 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users.js");
 const authRoute = require("./routes/auth.js");
 const postRoute = require("./routes/post.js");
-
+const multer = require("multer");
+const path = require("path");
+const cors =require("cors");
 
 // Set Mongoose to use built-in JavaScript Promises
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://website2522:25222522@cluster0.mp2pckn.mongodb.net/Social?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  "mongodb+srv://website2522:25222522@cluster0.mp2pckn.mongodb.net/Social?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+fetch('http://localhost:8800', { mode: 'no-cors' });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+
+});
 
 // Use the user route for "/api/users"
 app.use("/api/users", userRoute);
@@ -25,5 +51,5 @@ app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
 
 app.listen(8800, () => {
-  console.log("Backend is running!");    
+  console.log("Backend is running!");
 });

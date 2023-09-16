8f14c0b6d622d8658352f977ca11bef06857c39a
HarshMehta2522 <maheta.hrs2522@gmail.com>
2023-09-16 23:37:55 +0530

    get conversation updated

--- routes/conversations.js
+++ routes/conversations.js
@@ -3,16 +3,30 @@ const Conversation = require("../models/Conversation");
 const conversation = require("../models/Conversation");
 
 router.post("/", async (req, res) => {
-  const newConversation = new conversation({
-    members: [req.body.sender, req.body.receiverId],
-  });
   try {
-    const savedConversation = await newConversation.save();
-    res.status(200).json(savedConversation);
+    // Check if a conversation already exists with the same members
+    const existingConversation = await Conversation.findOne({
+      members: {
+        $all: [req.body.sender, req.body.receiverId],
+      },
+    });
+
+    if (existingConversation) {
+      // If a conversation already exists, return it
+      res.status(201).json(existingConversation);
+    } else {
+      // If no conversation exists, create a new one
+      const newConversation = new Conversation({
+        members: [req.body.sender, req.body.receiverId],
+      });
+      const savedConversation = await newConversation.save();
+      res.status(200).json(savedConversation);
+    }
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
 router.get("/:userId", async (req, res) => {
   try {
     const conversation = await Conversation.find({
